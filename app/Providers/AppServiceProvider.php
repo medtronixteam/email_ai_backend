da04fc69bc46e1ced047ae8f7215e14c2f42d30e
Rana Shehroz <ranashahroz.shabbir786@gmail.com>
2024-10-17 19:02:37 +0500

    Production Mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        if (env('APP_ENV') == 'local') {
             URL::forceScheme('https');
         }
 
