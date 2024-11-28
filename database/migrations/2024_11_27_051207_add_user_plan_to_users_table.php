6ab2b0b6e3a2f3748c8d01a9be8595a619334c93
zeeshanali90 <158147792+zeeshanali90@users.noreply.github.com>
2024-11-27 21:01:24 -0800

    ..

--- database/migrations/2024_11_27_051207_add_user_plan_to_users_table.php
+++ database/migrations/2024_11_27_051207_add_user_plan_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('user_plan')->default('user');
+            $table->string('user_plan')->default('free');
         });
     }
 
